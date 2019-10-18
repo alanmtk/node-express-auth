382c577fa70d73d3684f44ba08c8b837b5b323e8
Alan Matkorski <alanmatkorski@gmail.com>
2019-10-17 22:14:13 -0300

    extracted auth logic to a controller file

---
+++ server/src/auth/auth.controller.js
@@ -0,0 +1,27 @@
+const bcrypt = require('bcrypt');
+const userSchema = require('./auth.schema');
+const db = require('./../db');
+
+const users = db.get('users');
+
+const signUp = async (req, res, next) => {
+    const userValidation = userSchema.validate(req.body);
+    if (!userValidation.error) {
+        const { username, email, password } = req.body;
+        const userExists = await users.findOne({ email });
+        if (!userExists) {
+            const hashedPassword = await bcrypt.hash(password, 12);
+            const user = await users.insert({ username, email, password: hashedPassword });
+            delete user.password;
+            res.send(user);
+        } else {
+            res.statusCode = 409;
+            next(new Error('That email was taken by another user.'));
+        }
+    } else {
+        res.statusCode = 422;
+        next(new Error(userValidation.error.message));
+    }
+};
+
+module.exports = { signUp };
